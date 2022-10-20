61e41040648fe7cf3fe3f1aad869549b37c3c967
Phong Hoang <phonghoang1023@gmail.com>
2022-10-20 13:58:10 +0700

    delete unused res

--- admob/src/main/java/com/smartmobile/example/TestAdId.kt
+++ admob/src/main/java/com/smartmobile/example/TestAdId.kt
@@ -4,6 +4,4 @@ object TestAdId {
     const val INTER_ID = "ca-app-pub-3940256099942544/1033173712"
     const val BANNER_ID = "ca-app-pub-3940256099942544/6300978111"
     const val NATIVE_ID = "ca-app-pub-3940256099942544/2247696110"
-
-
 }
